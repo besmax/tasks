c4905e39b9566136e0a91ddb214df101fd58485f
Max <bespalov.m.9@ya.ru>
2023-02-08 21:02:30 +0300

    TasksFragment and fragment_tasks were populated

---
+++ app/src/main/java/bes/max/tasks/TasksFragment.kt
@@ -0,0 +1,29 @@
+package bes.max.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class TasksFragment : Fragment() {
+
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
