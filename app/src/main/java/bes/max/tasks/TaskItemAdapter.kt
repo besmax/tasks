7cd52d2fb1baa03fc5f94c08036c9258fcafa705
Max <bespalov.m.9@ya.ru>
2023-02-19 10:50:34 +0300

    data binding in recycler view was added

--- app/src/main/java/bes/max/tasks/TaskItemAdapter.kt
+++ app/src/main/java/bes/max/tasks/TaskItemAdapter.kt
@@ -2,11 +2,9 @@ package bes.max.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
 import androidx.recyclerview.widget.ListAdapter
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import bes.max.tasks.databinding.TaskItemBinding
 
 class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
 
@@ -18,22 +16,17 @@ class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(Ta
         holder.bind(item)
     }
 
-    class TaskItemViewHolder(val rootView : CardView) : RecyclerView.ViewHolder(rootView) {
-
-        val taskName = rootView.findViewById<TextView>(R.id.task_name)
-        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+    class TaskItemViewHolder(val binding: TaskItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         companion object {
             fun inflateFrom(parent : ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.task_item, parent, false) as CardView
-                return TaskItemViewHolder(view)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return TaskItemViewHolder(binding)
             }
         }
         fun bind(item: Task) {
-            taskName.text = item.taskName
-            taskDone.isChecked = item.taskDone
+            binding.task = item
         }
     }
 }
